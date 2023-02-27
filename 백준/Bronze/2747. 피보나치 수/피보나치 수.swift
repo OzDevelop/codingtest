08d0a73f4a240801550d4be7d1caaf129e9d8518
sangpil byeon <byunsp3610@gmail.com>
2023-02-27 21:15:22 +0900

    Update 피보나치 수.swift
    첫번째 재귀함수를 이용한 코드는 시간 초과
    두번째 코드는 왜 틀렸는지 모르겠음
    세번째는 인터넷 봤는데 진짜 왜 틀렸지...?

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.swift
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.swift
@@ -1,4 +1,44 @@
+//
+//  main.swift
+//  2747_피보나치 수
+//
+//  Created by 변상필 on 2023/02/27.
+//
+
 import Foundation
+
+
+// 시간초과
+//let n = Int(readLine()!)!
+//func Fibonacci(_ n: Int) -> Int {
+//    if n <= 1 {
+//        return n
+//    }
+//    return Fibonacci(n-1) + Fibonacci(n-2)
+//}
+//
+//print(Fibonacci(n))
+
+
+//이건 왜틀렸는지 모르겠음 ;;
+//let n = Int(readLine()!)!
+//func Fibonacci(_ n: Int){
+//    var a = 0, b = 1
+//    if n <= 1 {
+//        print(n)
+//    }
+//    for _ in 0..<n {
+//        let temp = a
+//        a = b
+//        b = temp + b
+//        //print(a)
+//        //print(i)
+//    }
+//    print(a)
+//}
+//Fibonacci(n)
+
+//이건 인터넷에서 가져온거
 func fibonacci(_ n: Int) -> Int {
     var n = n, a = 0, b = 1
     var temp = a

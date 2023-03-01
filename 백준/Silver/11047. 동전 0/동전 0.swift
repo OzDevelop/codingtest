5c67aed10e2cbdd392ec361a0017c7d8f6402094
sangpil byeon <byunsp3610@gmail.com>
2023-03-01 21:11:19 +0900

    [Silver IV] Title: 동전 0, Time: 12 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.swift
@@ -0,0 +1,26 @@
+import Foundation
+
+let nk = readLine()!.split(separator: " ").map{ Int(String($0))!}
+let n = nk[0]
+var k = nk[1]
+
+var nlist: Array<Int> = []
+var result = 0
+
+for _ in 0..<n {
+    nlist.append(Int(readLine()!)!)
+}
+
+for i in nlist.reversed() {
+    if i <= k{
+        result += (k/i)
+        k = k%i
+        
+    } else if k == 0 {
+        break
+    }
+}
+print(result)
+
+//print(nlist)
+                    

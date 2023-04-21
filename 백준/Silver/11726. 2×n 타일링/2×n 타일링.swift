8a83f3d5eb03bd8c36e15f681d51000d681bc012
sangpil byeon <byunsp3610@gmail.com>
2023-04-21 13:27:33 +0900

    [Silver III] Title: 2×n 타일링, Time: 40 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+let input = Int(readLine()!)!
+var dp : [Int] = Array(repeating: 0, count: input+1)
+
+if input == 1 {
+    print(1)
+} else if input == 2 {
+    print(2)
+} else {
+    dp[1] = 1
+    dp[2] = 2
+
+    for i in 3..<input+1 {
+        dp[i] = (dp[i-1] + dp[i-2])%10_007
+    }
+    print(dp[input])
+
+}

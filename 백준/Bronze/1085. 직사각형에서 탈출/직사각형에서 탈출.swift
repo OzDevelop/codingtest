9a7627b675c9e5ac66eb6c933ce085da352be728
sangpil byeon <byunsp3610@gmail.com>
2023-04-27 00:46:56 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 12 ms, Memory: 69104 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.swift
@@ -0,0 +1,10 @@
+var arr = readLine()!.split(separator: " ").map{Int($0)!}
+arr[2] = arr[2] - arr[0]
+arr[3] = arr[3] - arr[1]
+var temp = arr[0]
+for i in 0..<arr.count {
+    if temp > arr[i] {
+        temp = arr[i]
+    }
+}
+print("\(temp)")

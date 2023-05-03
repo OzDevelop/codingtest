42a404825ea38660d5bbfd376bb71f2e52aaf452
sangpil byeon <byunsp3610@gmail.com>
2023-05-03 17:36:05 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  11727_2xn타일링2
+//
+//  Created by 변상필 on 2023/05/03.
+//
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+var dp : [Int] = Array(repeating: 0, count: n+1)
+
+if n == 1 {
+    print(1)
+}
+if n == 2 {
+    print(3)
+}
+if n >= 3 {
+    dp[0] = 0
+    dp[1] = 1
+    dp[2] = 3
+    for i in 3..<n+1 {
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10_007
+        
+    }
+    print(dp[n])
+}

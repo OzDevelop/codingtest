5327b826a81c43ed6ccaf70c5d3b162994257b7d
sangpil byeon <byunsp3610@gmail.com>
2023-05-18 16:47:57 +0900

    [unrated] Title: 조건 문자열, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ ineq:String, _ eq:String, _ n:Int, _ m:Int) -> Int {
+    var result = 0
+    if ineq == ">" && eq == "=" {
+        if (n >= m ) { result = 1}
+    } else if ineq == ">" && eq == "!" {
+        if (n > m) { result = 1}
+    } else if ineq == "<" && eq == "=" {
+        if (n <= m) { result = 1} 
+    } else if ineq == "<" && eq == "!" {
+        if ( n < m ) { result = 1}
+    } else { result = 0}
+    return result
+}

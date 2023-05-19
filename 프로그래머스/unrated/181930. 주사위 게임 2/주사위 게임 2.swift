a87830184954117e141d54b34100764f2853cc17
sangpil byeon <byunsp3610@gmail.com>
2023-05-19 20:43:14 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.swift
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.swift
@@ -1,18 +1,14 @@
 import Foundation
 func solution(_ a:Int, _ b:Int, _ c:Int) -> Int {
     var result = 0
-    var result1 = pow(Float(a), 2) + pow(Float(b),2) + pow(Float(c),2)
-    var result2 = (pow(Float(a),3) + pow(Float(b),3) + pow(Float(c),3))
+    let result1 = a*a + b*b + c*c
+    var result2 = a*a*a + b*b*b + c*c*c
     if a != b && a != c && b != c {
         return a+b+c
-        print("1",result1)
-        print(result2)
-
     }else if a == b && b == c {
-        result = (a + b + c) * Int(result1) * Int(result2)
+        result = (a + b + c) * result1 * result2
     }else if a == b || a == c || b == c {
-        result = Int(a + b + c) * Int(result1)
+        result = Int(a + b + c) * result1
     }
     return result
-
 }

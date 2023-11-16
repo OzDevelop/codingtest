9261e9c3513eca507bdb202d47a93ca6febfd782
sangpil byeon <byunsp3610@gmail.com>
2023-11-16 19:02:41 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func solution(_ food:[Int]) -> String {
+    var ans = ""
+    for i in food.indices {
+        ans += String(repeating: String(i), count: food[i]/2)
+    }
+    ans = ans + String(0) + ans.reversed()
+    return ans
+}

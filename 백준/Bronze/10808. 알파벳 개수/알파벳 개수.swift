a6884444f8534c5b9df6a552c89fc3c170ea15c2
sangpil byeon <byunsp3610@gmail.com>
2024-07-14 16:42:27 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.swift
@@ -1,9 +1,10 @@
 import Foundation
 
-let s = readLine()!
-var alphabet = [Int](repeating: 0, count: 26)
+let input = readLine()!
+var ans = Array(repeating: 0, count: 26)
 
-for i in s {
-    alphabet[Int(i.asciiValue!)-97] += 1
+for char in input {
+    ans[Int(char.asciiValue!) - 97] += 1
 }
-print(alphabet.map{ "\($0)"}.joined(separator:" "))
+
+print(ans.map{String($0)}.joined(separator: " "))

b9dd12104c0839f37e176adc811e33876694a8ac
sangpil byeon <byunsp3610@gmail.com>
2024-07-19 21:46:45 +0900

    [Bronze III] Title: 네 수, Time: 8 ms, Memory: 69104 KB -BaekjoonHub

--- 백준/Bronze/10824. 네 수/네 수.swift
+++ 백준/Bronze/10824. 네 수/네 수.swift
@@ -1,7 +1,4 @@
-import Foundation
+let input = readLine().map{String($0)}!.split(separator: " ")
+var ans =  Int(input[0] + input[1])! + Int(input[2] + input[3])!
 
-let input = readLine()!.split(separator:" ").map { Int(String($0))!}
-let num1 = String(input[0]) + String(input[1])
-let num2 = String(input[2]) + String(input[3])
-print(Int(num1)! + Int(num2)!)
-
+print(ans)

df76f80a419d7592a20dbe99edc0d9c5fd542b08
sangpil byeon <byunsp3610@gmail.com>
2024-07-19 21:37:35 +0900

    [Bronze I] Title: ROT13, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.swift
+++ 백준/Bronze/11655. ROT13/ROT13.swift
@@ -1,22 +1,27 @@
-import Foundation
+let input = readLine()!
+var ans = ""
 
-let word = Array(readLine()!)
-var wordRot = ""
-
-for i in word {
-    var tmp = i.asciiValue!
-    if tmp >= 65 && tmp <= 90{
-        tmp += 13
-        if tmp > 90 {
-            tmp -= 26
-        }
-    } else if tmp >= 97 && tmp <= 122  {
-        tmp += 13
-        if tmp > 122 {
-            tmp -= 26
-        }
-    }
-    wordRot += String(UnicodeScalar(tmp))
+for i in input {
+    let asciiChar = i.asciiValue!
+    let overChar = String(UnicodeScalar(asciiChar - 13))
+    let underChar = String(UnicodeScalar(asciiChar + 13))
     
+    if i.isLowercase {
+        if asciiChar + 13 > 122 {
+            ans.append(overChar)
+            continue
+        }
+        ans.append(underChar)
+        
+    } else if i.isUppercase {
+        if asciiChar + 13 > 90 {
+            ans.append(overChar)
+            continue
+        }
+        ans.append(underChar)
+    } else {
+        ans.append(i)
+    }
 }
-print(wordRot)
+
+print(ans)

e172be2bad34f485812c9cfa8307f71084877020
sangpil byeon <byunsp3610@gmail.com>
2024-08-08 22:10:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 8 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.swift
@@ -1,48 +1,26 @@
 import Foundation
 
-// 이건 왜 안되냐?
-//func solution ()-> Void {
-//    let n = Int(readLine()!)!
-//
-//    for _ in 0..<n{
-//        let num = Int(readLine()!)!
-//        var dp = [Int](repeating: 0, count: 12)
-//        dp[1] = 1
-//        dp[2] = 2
-//        dp[3] = 4
-//
-//        if num >= 4 {
-//            for i in 4 ..< num+1 {
-//                dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
-//            }
-//            print(dp[num])
-//        }
-//    }
-//}
-//
-//solution()
-
 let n = Int(readLine()!)!
 
 for _ in 0..<n {
-    let num = Int(readLine()!)!
-    var dp = [Int](repeating: 0, count: num+1)
+    let input = Int(readLine()!)!
+    var dp: [Int] = Array(repeating: 0, count: input + 1)
     
-    if num == 1 {
+    switch input {
+    case 0:
+        print(0)
+    case 1:
         print(1)
-    } else if num == 2 {
+    case 2:
         print(2)
-    } else if num == 3 {
+    case 3:
         print(4)
-    } else {
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
-
-        for i in 4..<num+1 {
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+    default:
+        dp[0] = 0; dp[1] = 1; dp[2] = 2; dp[3] = 4
+        
+        for j in 4..<input + 1{
+            dp[j] = dp[j-1] + dp[j-2] + dp[j-3]
         }
-        print(dp[num])
+        print(dp[input])
     }
-
 }

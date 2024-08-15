c73b093eea702ca40db9a9b58d61c96acf831500
sangpil byeon <byunsp3610@gmail.com>
2024-08-15 22:27:40 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 16 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.swift
@@ -1,12 +1,13 @@
 import Foundation
 
-let n = Int(readLine()!)!
-let nArr = readLine()!.split(separator: " ").map{ Int(String($0))!}
-var dp = [Int](Array(repeating: 1, count: n+1))
+let input = Int(readLine()!)!
+let inputArr = readLine()!.split(separator: " ").map {
+    Int(String($0))! }
+var dp = Array(repeating: 1, count: input+1)
 
-for i in 1..<nArr.count {
+for i in 1..<inputArr.count {
     for j in 0..<i {
-        if nArr[i] > nArr[j] {
+        if inputArr[i] > inputArr[j] {
             dp[i] = max(dp[i], dp[j] + 1)
         }
     }

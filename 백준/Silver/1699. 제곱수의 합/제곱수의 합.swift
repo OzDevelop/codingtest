1c8565c5e107574a834ce2d0103320d6eb352635
sangpil byeon <byunsp3610@gmail.com>
2024-08-19 22:16:16 +0900

    [Silver II] Title: 제곱수의 합, Time: 336 ms, Memory: 80292 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.swift
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.swift
@@ -1,16 +1,15 @@
 import Foundation
 
-let n = Int(readLine()!)!
-var dp: [Int] = Array(repeating: 0, count: 100001)
+let input = Int(readLine()!)!
+var dp = Array(repeating: 0, count: input+1)
 
-for i in stride(from: 1, through: n, by: 1) {
+for i in 1..<input+1 {
     dp[i] = i
-    for j in stride(from: 1, through: i, by: 1) {
+    for j in 1..<i {
         if j*j > i {
             break
         }
-        dp[i] = min(dp[i], dp[i - j * j] + 1)
+        dp[i] = min(dp[i], dp[i-j*j]+1)
     }
 }
-print(dp[n])
-        
+print(dp[input])

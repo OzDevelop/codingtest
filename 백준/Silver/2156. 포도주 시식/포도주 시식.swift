fd7c5b4f5980efe5d258440d7c8c6156ff2246bb
sangpil byeon <byunsp3610@gmail.com>
2024-09-19 23:00:01 +0900

    [Silver I] Title: 포도주 시식, Time: 8 ms, Memory: 69380 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.swift
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.swift
@@ -1,18 +1,28 @@
-let n = Int(String(readLine()!))!
-var arr:[Int] = []
-var dp = Array(repeating: 0, count: 10001)
-arr.append(0)
-for _ in 1...n {
-    arr.append(Int(String(readLine()!))!)
+let n = Int(readLine()!)!
+var juices: [Int] = []
+// dp는 i번째까지 마신 경우 중 최대값을 저장
+var dp: [Int] = Array(repeating: 0, count: n+1)
+
+juices.append(0)
+for _ in 1..<n+1 {
+    juices.append(Int(readLine()!)!)
 }
 
-dp[1] = arr[1]
-if n >= 2 {
-    dp[2] = arr[1] + arr[2]
+dp[1] = juices[1]
+if n>1 {
+    dp[2] = juices[2] + juices[1]
+}
+if n>2 {
+    /// i 번째 포도주를 마시고, i-1 번째 포도주도 마시는 경우
+    /// dp[i-3]+juices[i-1]+juices[i]
+    /// i 번째 포도주를 마시고, i-1 번째 포도주는 마시지 않는 경우
+    /// dp[i-2]+dp[i]
+    /// i번째 포도주를 마시지 않는 경우
+    /// dp[i-1]
+    for i in 3..<n+1 {
+        dp[i] = max(dp[i-3]+juices[i-1]+juices[i], dp[i-2]+juices[i], dp[i-1])
+    }
 }
 
-for i in stride(from: 3, through: n, by: 1) {
-    dp[i] = max(dp[i - 2] + arr[i], dp[i - 3] + arr[i] + arr[i - 1], dp[i - 1])
-}
-
+//print(juices)
 print(dp[n])

290b4b5f22a32c052f2bf3c0238facd310c22191
sangpil byeon <byunsp3610@gmail.com>
2024-12-29 21:43:02 +0900

    [Bronze I] Title: 성 지키기, Time: 180 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Bronze/1236. 성 지키기/성 지키기.java
+++ 백준/Bronze/1236. 성 지키기/성 지키기.java
@@ -15,33 +15,52 @@ public class Main {
         }
 
         // 1. 각 행/열에 대해 경비원이 있는지 확인한다.
-        int existRowCount = 0;
+        boolean[] existRow = new boolean[N];
+        boolean[] existCol = new boolean[M];
         for (int i = 0; i < N; i++) {
-            boolean exist = false;
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == 'X') {
-                    exist = true;
-                    break;
+                if(map[i][j] == 'X') {
+                    existRow[i] = true;
+                    existCol[j] = true;
                 }
             }
-            if (exist) existRowCount++;
         }
+
+        int existRowCount = 0;
+//        for (int i = 0; i < N; i++) {
+//            boolean exist = false;
+//            for (int j = 0; j < M; j++) {
+//                if (map[i][j] == 'X') {
+//                    exist = true;
+//                    break;
+//                }
+//            }
+//            if (exist) existRowCount++;
+//        }
 
         int existColCount = 0;
-        for (int i = 0; i < M; i++) {
-            boolean exist = false;
-            for (int j = 0; j < N; j ++) {
-                if (map[j][i] == 'X') {
-                    exist = true;
-                    break;
-                }
-            }
-            if (exist) existColCount++;
-        }
+//        for (int i = 0; i < M; i++) {
+//            boolean exist = false;
+//            for (int j = 0; j < N; j ++) {
+//                if (map[j][i] == 'X') {
+//                    exist = true;
+//                    break;
+//                }
+//            }
+//            if (exist) existColCount++;
+//        }
 
         // 2. 보호받지 못하는 행/열의 개수를 구한다.
-        int needRowCount = N - existRowCount;
-        int needColCount = M - existColCount;
+        int needRowCount = N;
+        int needColCount = M;
+
+        for (int i = 0; i < N; i++) {
+            if (existRow[i]) needRowCount--;
+        }
+
+        for (int i = 0; i < M; i++) {
+            if (existCol[i]) needColCount--;
+        }
 
         // 3. 둘 중 큰 값을 출력한다.
         System.out.println(Math.max(needColCount, needRowCount));

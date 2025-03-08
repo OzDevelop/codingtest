0276781de05717bab0344e51acfc1a71cfa9a6bc
sangpil byeon <byunsp3610@gmail.com>
2025-03-08 12:11:06 +0900

    [Silver V] Title: 나이순 정렬, Time: 1760 ms, Memory: 146844 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,19 +1,26 @@
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.Scanner;
 
+// Comparator 대신 Comparable을 구현해 사용
 public class Main {
-    public static class userInfo {
-        static Long count = 1L;
-        Long id;
+    static class Member implements Comparable<Member> {
         int age;
         String name;
+        int idx;
 
-        public userInfo(int age, String name) {
-            this.id = count++;
+        public Member(int age, String name, int idx) {
             this.age = age;
             this.name = name;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Member o) {
+            if (this.age != o.age) {
+                return age - o.age;
+            }
+            return idx - o.idx;
         }
     }
 
@@ -21,24 +28,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        userInfo[] user = new userInfo[N];
+        Member[] members = new Member[N];
 
         for (int i = 0; i < N; i++) {
             int age = sc.nextInt();
             String name = sc.next();
-            user[i] = new userInfo(age, name);
+            members[i] = new Member(age, name, i);
         }
 
-        Arrays.sort(user, (o1, o2) -> {
-            if (o1.age == o2.age) {
-                return Long.compare(o1.id, o2.id);
-            }
-            return Integer.compare(o1.age, o2.age);
-        });
+//        Arrays.sort(members, (o1, o2) -> {
+//            if (o1.age == o2.age) {
+//                return Long.compare(o1.idx, o2.idx);  // id를 기준으로 정렬
+//            }
+//            return Integer.compare(o1.age, o2.age);  // 나이를 기준으로 오름차순 정렬
+//        });
 
+        Arrays.sort(members);
 
-        for (userInfo userInfo : user) {
-            System.out.println(userInfo.age + " " + userInfo.name);
+        for (Member Member : members) {
+            System.out.println(Member.age + " " + Member.name);
         }
     }
 }

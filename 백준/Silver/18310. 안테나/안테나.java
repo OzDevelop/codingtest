ef43ab0d2cba0ce3c35b5672f59d7404b09d4bee
sangpil byeon <byunsp3610@gmail.com>
2025-03-27 20:41:14 +0900

    [Silver III] Title: 안테나, Time: 1168 ms, Memory: 177772 KB -BaekjoonHub

---
+++ 백준/Silver/18310. 안테나/안테나.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] xs = new int[N];
+        for (int i = 0; i < N; i++)
+            xs[i] = sc.nextInt();
+
+        Arrays.sort(xs);
+
+        System.out.println(xs[(N - 1) / 2]);
+    }
+}

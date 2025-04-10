dfc4dace1ae11d4326f1548cc3b91672495d59e7
sangpil byeon <byunsp3610@gmail.com>
2025-04-10 23:06:40 +0900

    [Silver II] Title: Generic Queries, Time: 5260 ms, Memory: 318348 KB -BaekjoonHub

--- 백준/Silver/16713. Generic Queries/Generic Queries.java
+++ 백준/Silver/16713. Generic Queries/Generic Queries.java
@@ -7,24 +7,47 @@ public class Main {
         int N = sc.nextInt();
         int Q = sc.nextInt();
 
-        int[] prefixXOR = new int[N + 1];
+        int[] arr = new int[N+1];
 
+        // 강의 풀이
         for (int i = 1; i <= N; i++) {
-            int num = sc.nextInt();
-            prefixXOR[i] = prefixXOR[i - 1] ^ num;
+            arr[i] = sc.nextInt();
         }
 
-        int result = 0;
-
-        for (int i = 0; i < Q; i++) {
-            int left = sc.nextInt();
-            int right = sc.nextInt();
-
-            result ^= (prefixXOR[right] ^ prefixXOR[left - 1]);
+        // 1. 누적 xor 배열을 구함.
+        int[] acc = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            acc[i] = acc[i-1] ^ arr[i];
         }
 
-        System.out.println(result);
+        // 2. Q번 [s:e] 질문에 대해 누적XOR 배열을 사용해 구간 XOR을 구한다.
+        int ans = 0;
+        while(Q-- > 0) {
+            int s = sc.nextInt();
+            int e = sc.nextInt();
+            ans ^= acc[e] ^ acc[s - 1];
+        }
+
+        System.out.println(ans);
+
+
+        // 내 풀이
+//        int[] prefixXOR = new int[N + 1];
+
+//        for (int i = 1; i <= N; i++) {
+//            int num = sc.nextInt();
+//            prefixXOR[i] = prefixXOR[i - 1] ^ num;
+//        }
+//
+//        int result = 0;
+//
+//        for (int i = 0; i < Q; i++) {
+//            int left = sc.nextInt();
+//            int right = sc.nextInt();
+//
+//            result ^= (prefixXOR[right] ^ prefixXOR[left - 1]);
+//        }
+//
+//        System.out.println(result);
     }
 }
-
-

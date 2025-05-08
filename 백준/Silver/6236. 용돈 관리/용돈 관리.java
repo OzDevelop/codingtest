526567c847510b10b1107100c511b0a96d4ec9d3
sangpil byeon <byunsp3610@gmail.com>
2025-05-08 09:07:42 +0900

    [Silver I] Title: 용돈 관리, Time: 256 ms, Memory: 23376 KB -BaekjoonHub

---
+++ 백준/Silver/6236. 용돈 관리/용돈 관리.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[] budget;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+
+        budget = new int[N];
+        for(int i = 0; i < N; i++) {
+            budget[i] = Integer.parseInt(br.readLine());
+        }
+        System.out.println(getMinWithdrawMoney(budget));
+
+    }
+
+    private static int getMinWithdrawMoney(int[] budget) {
+        int l = Arrays.stream(budget).max().getAsInt(), r = Arrays.stream(budget).sum();
+        int ans = 0;
+
+        while(l <= r) {
+            int m = (l + r) / 2;
+
+            if(isValid(m) <= M) {
+                ans = m;
+                r = m - 1;
+            } else if (isValid(m)  > M)
+                l = m + 1;
+
+        }
+        return ans;
+    }
+
+    private static int isValid(int m) {
+        int sum = m, count = 1;
+
+        for(int money: budget) {
+            if (sum < money) {
+                sum = 0;
+                sum += m;
+                count++;
+            }
+            sum -= money;
+        }
+        return count;
+    }
+}

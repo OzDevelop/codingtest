bee1f788c13573e5da2b875b2af66c6de67c16ba
sangpil byeon <byunsp3610@gmail.com>
2025-05-12 07:46:08 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 24240 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        int ans = Integer.MAX_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i ++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int left = 0, right = 0, sum = 0; // 투 포인터에 사용할 인덱스와 합
+
+        while (right < N) {
+            while (sum < S && right < N) {
+                sum += arr[right++];
+            }
+
+            while (sum >= S && left <= right) {
+                ans = Math.min(ans, right - left);
+                sum -= arr[left++];
+            }
+        }
+
+        System.out.println(ans == Integer.MAX_VALUE ? 0 : ans);
+    }
+}

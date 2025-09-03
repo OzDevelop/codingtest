e5bb3fc468ffdc500a92393c12e88bd1108d4ecf
sangpil byeon <byunsp3610@gmail.com>
2025-09-04 07:01:41 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        answer[0] = gcd(n, m);
+        answer[1] = (n * m) / gcd(n, m);
+        return answer;
+    }
+    
+    private int gcd(int a, int b) {
+        if(a % b == 0 ) return b;
+        return gcd(b, a%b);
+    }
+}
